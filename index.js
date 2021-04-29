1abd69368d73ee28d443e63316669ada2346b680
jackcornblum <jcornblum@gmail.com>
2021-04-29 12:47:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event) {
+    const congrats = []
+    for (let i = 0; i < names.length; i++) {
+        congrats.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return congrats;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "surprise" );
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(10);
